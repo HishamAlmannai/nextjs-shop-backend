e63a3f5927e0113f768123f76c123e602e4b532b
Hisham Almannai <hisham@gmx.de>
2022-05-06 17:57:39 +0200

    Correct edit method

--- pages/api/category/edit.js
+++ pages/api/category/edit.js
@@ -6,14 +6,10 @@ export default async function handler(request, response) {
     const data = JSON.parse(request.body);
     await dbConnect();
 
-    let category = await Category.findOne({ name: data.name });
-
-    if (!category) {
-      category = await Category.findByIdAndUpdate(data.id, {
-        name: data.name,
-        description: data.description,
-      });
-    }
+    const category = await Category.findByIdAndUpdate(data.id, {
+      name: data.name,
+      description: data.description,
+    });
 
     response.status(200).json({
       message: "Category edited",
