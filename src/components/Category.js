b63cd555c31d656e98768922df1ae19b0f1aba78
Hisham Almannai <hisham@gmx.de>
2022-11-30 11:34:52 +0100

    Set ternary categories

--- src/components/Category.js
+++ src/components/Category.js
@@ -10,54 +10,66 @@ import { useState } from "react";
 import { mutate } from "swr";
 
 export default function Category({ id, name, description }) {
-  const [nameValue, setNameValue] = useState("");
-  const [descriptionValue, setDescriptionValue] = useState("");
+  const [nameValue, setNameValue] = useState(name);
+  const [descriptionValue, setDescriptionValue] = useState(description);
+  const [editValue, setEditValue] = useState(false);
 
   const submit = async (event) => {
     event.preventDefault();
-
-    const response = await fetch("/api/category/edit", {
-      method: "PUT",
-      body: JSON.stringify({
-        id,
-        name: nameValue,
-        description: descriptionValue,
-      }),
-    });
-    mutate("/api/categories");
+    if (editValue) {
+      const response = await fetch("/api/category/edit", {
+        method: "PUT",
+        body: JSON.stringify({
+          id,
+          name: nameValue,
+          description: descriptionValue,
+        }),
+      });
+      mutate("/api/categories");
+      setEditValue(!editValue);
+    } else {
+      setEditValue(!editValue);
+      console.log(editValue);
+    }
   };
 
   return (
     <Card>
       <CardContent>
-        <Typography variant="h5">{name}</Typography>
-        <TextField
-          name="name"
-          label="name"
-          required
-          fullWidth
-          sx={{ marginTop: 1.5 }}
-          value={nameValue}
-          onChange={(event) => {
-            setNameValue(event.target.value);
-          }}
-        />
-        <Typography variant="h5">{description}</Typography>
-        <TextField
-          name="description"
-          label="description"
-          required
-          fullWidth
-          sx={{ marginTop: 1.5 }}
-          value={descriptionValue}
-          onChange={(event) => {
-            setDescriptionValue(event.target.value);
-          }}
-        />
+        {!editValue ? (
+          <Typography variant="h5">{name}</Typography>
+        ) : (
+          <TextField
+            name="name"
+            label="name"
+            required
+            fullWidth
+            sx={{ marginTop: 1.5 }}
+            value={nameValue}
+            onChange={(event) => {
+              setNameValue(event.target.value);
+            }}
+          />
+        )}
+        {!editValue ? (
+          <Typography variant="h5">{description}</Typography>
+        ) : (
+          <TextField
+            name="description"
+            label="description"
+            required
+            fullWidth
+            sx={{ marginTop: 1.5 }}
+            value={descriptionValue}
+            onChange={(event) => {
+              setDescriptionValue(event.target.value);
+            }}
+          />
+        )}
       </CardContent>
       <CardActions>
         <Button size="small" onClick={submit}>
-          Edit
+          {!editValue ? "Edit" : "Save"}
         </Button>
         <Button size="small">Delete</Button>
       </CardActions>
