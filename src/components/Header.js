fedba8965934f12de52732be8b4bd9d00ce1a573
Hisham Almannai <hisham@gmx.de>
2023-01-10 17:31:00 +0100

    Try css condition

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,100 +1,42 @@
-import {
-  AppBar,
-  Button,
-  Container,
-  ToggleButton,
-  ToggleButtonGroup,
-  Toolbar,
-} from "@mui/material";
+import { AppBar, Button, Container, Toolbar } from "@mui/material";
 import Link from "next/link";
 import { useState } from "react";
 
 export default function Header() {
-  const [selectedProducts, setSelectedProducts] = useState(false);
-  const [selectedCategories, setSelectedCategories] = useState(false);
-  const [selectedCreateProduct, setSelectedCreateProduct] = useState(false);
-  const [selectedCreateCategory, setSelectedCreateCategory] = useState(false);
-
- /*  if (selectedProducts===true) {
-    setSelectedCategories(false);
-    setSelectedCreateProduct(false);
-    setSelectedCreateProduct(false);
-  }
-  else if (selectedCategories===true) {
-    setSelectedProducts(false);
-    setSelectedCreateProduct(false);
-    setSelectedCreateProduct(false);
-  }
-  else if (selectedCreateProduct===true) {
-    setSelectedCategories(false);
-    setSelectedProducts(false);
-    setSelectedCreateProduct(false);
-  }
-  else if (selectedCreateProduct===true) {
-    setSelectedCategories(false);
-    setSelectedCreateProduct(false);
-    setSelectedProducts(false);
-  }; */
+  const [selected, setSelected] = useState();
 
   const buttonStyling = {
     color: "white",
     border: "none",
+    '&:target': 
+    {color: "black"},
   };
+
 
   return (
     <AppBar color="primary">
       <Container maxWidth="xl">
         <Toolbar>
-          <ToggleButtonGroup exclusive>
-            <Link href="/products" passHref>
-              <ToggleButton
-                component="a"
-                sx={buttonStyling}
-                selected={selectedProducts}
-                onClick={() => {
-                  setSelectedProducts(!selectedProducts);
-                }}
-              >
-                Produkte
-              </ToggleButton>
-            </Link>
-            <Link href="/categories" passHref>
-              <ToggleButton
-                component="a"
-                sx={buttonStyling}
-                selected={selectedCategories}
-                onClick={() => {
-                  setSelectedCategories(!selectedCategories);
-                }}
-              >
-                Kategorien
-              </ToggleButton>
-            </Link>
-            <Link href="/create-product" passHref>
-              <ToggleButton
-                component="a"
-                sx={buttonStyling}
-                selected={selectedCreateProduct}
-                onClick={() => {
-                  setSelectedCreateProduct(!selectedCreateProduct);
-                }}
-              >
-                Produkt hinzufügen
-              </ToggleButton>
-            </Link>
-            <Link href="/create-category" passHref>
-              <ToggleButton
-                component="a"
-                sx={buttonStyling}
-                selected={selectedCreateCategory}
-                onClick={() => {
-                  setSelectedCreateCategory(!selectedCreateCategory);
-                }}
-              >
-                Kategorie hinzufügen
-              </ToggleButton>
-            </Link>
-          </ToggleButtonGroup>
+          <Link href="/products" passHref>
+            <Button component="a" sx={buttonStyling} value="products">
+              Produkte
+            </Button>
+          </Link>
+          <Link href="/categories" passHref>
+            <Button component="a" sx={buttonStyling} value="categories">
+              Kategorien
+            </Button>
+          </Link>
+          <Link href="/create-product" passHref>
+            <Button component="a" sx={buttonStyling} value="create-product">
+              Produkt hinzufügen
+            </Button>
+          </Link>
+          <Link href="/create-category" passHref>
+            <Button component="a" sx={buttonStyling} value="create-category">
+              Kategorie hinzufügen
+            </Button>
+          </Link>
         </Toolbar>
       </Container>
     </AppBar>
